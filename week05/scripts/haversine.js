cf5f088fd147e758e3eedf54c95696d11e7e5177
Josephine Phang <jphang75@gmail.com>
2020-12-07 22:20:18 +0800

    1020PM

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -29,7 +29,7 @@ function calculateDistances(lat, lon) {
     let d1 = haversine(lat, lon, 2.922562, 101.650965);// de pulze, cyberjaya
     let d2 = haversine(lat, lon, 3.073065, 101.607787);// Sunway Pyramid
     let d3 = haversine(lat, lon, 3.158761, 101.714524);// klcc
-    return [di, d2, d3];
+    return [d1, d2, d3];
 }
 
 let elLocate = document.getElementById("locate");
