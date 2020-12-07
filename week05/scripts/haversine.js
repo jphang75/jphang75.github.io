b2bd5b140c9f8802e6b29205942c9a7f503acd55
Josephine Phang <jphang75@gmail.com>
2020-12-07 22:07:16 +0800

    1007PM

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -1,6 +1,6 @@
 function haversine(lat1, lon1, lat2, lon2) {
     let R = 6371; //earth radius in KM
-    let dLat = (let2-let1).toRad();
+    let dLat = (lat2-lat1).toRad();
     let dLon = (lon2-lon1).toRad();
     lat1 = lat1.toRad();
     lat2 = lat2.toRad();
