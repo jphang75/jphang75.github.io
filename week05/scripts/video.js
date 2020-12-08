b0aafa49032f37021afc94987eb87d1225073e30
Josephine Phang <jphang75@gmail.com>
2020-12-08 20:45:06 +0800

    0845PM

---
+++ week05/scripts/video.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    let video = document.getElementById("myVideo");
+
+    let playBtn = document.getElementById("playBtn");
+    playBtn.addEventListener("click", function() {
+        video.play();
+    });
+
+    let pauseBtn = document.getElementById("pauseBtn");
+    pauseBtn.addEventListener("click", function() {
+        video.pause();
+    });
+
+    let freezeBtn = document.getElementById("freezeBtn");
+    freezeBtn.addEventListener("click", function() {
+        video.freeze();
+    });
+    
+
+};
