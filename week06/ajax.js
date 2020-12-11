638ca31a88eb75fb6f5de160f11bdcaaf6e5c5e5
Josephine Phang <jphang75@gmail.com>
2020-12-11 20:38:40 +0800

    0838PM

---
+++ week06/ajax.js
@@ -0,0 +1,27 @@
+function getRandomUser() {
+    let xhttp =  new XMLHttpRequest(); //create request obj
+
+    xhttp.onreadystatechange = function (){
+        if(this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.response);
+            let elFirstName = document.getElementById("firstName");
+            let ellastName = document.getElementById("lastName");
+            let eljsonResult = document.getElementById("jsonResult");
+            let elUserImage = document.getElementById("userImage");
+            let elPhone = document.getElementById("phone");
+            let elAddress = document.getElementById("address");
+            let elEmail = document.getElementById("email");
+
+            eljsonResult.innerHTML = this.response;
+        }
+
+    }
+
+    xhttp.open("GET", "https://randomuser.me/api/", true);
+    xhttp.send();
+}
+
+let elGetRandomUser = document.getElementById("getRandomUserBtn")
+elGetRandomUser.addEventListener("click", function() {
+    getRandomUser();
+});
