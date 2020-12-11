b4f49d440e4bfa0f633b8f6bc53e289d52122925
Josephine Phang <jphang75@gmail.com>
2020-12-11 20:59:33 +0800

    0859PM

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -12,7 +12,7 @@ function getRandomUser() {
             let elAddress = document.getElementById("address");
             let elEmail = document.getElementById("email");
 
-            elFirstName.innerHTML = data.result[0].name.first;
+            elFirstName.innerHTML = data.results[0].name.first;
 
             eljsonResult.innerHTML = this.response;
         }
