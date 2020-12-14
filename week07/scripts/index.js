e8c83d8c08ee5caace3fe7fdba68f9f31ccfd3bc
Josephine Phang <jphang75@gmail.com>
2020-12-14 20:46:15 +0800

    0846PM

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -1,7 +1,15 @@
-let app = new vue ({
+let app = new Vue ({
     el: "#app", 
     data: {
         message: "Hello Vue!"
+    }
+
+});
+
+let app2 = new Vue ({
+    el: "#app2", 
+    data: {
+        message: "You loaded this page on" + new Date().toLocaleString()
     }
 
 });
