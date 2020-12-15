e6d2c9baa11e0b8177005a2fb6f58c2681ac25ff
Josephine Phang <jphang75@gmail.com>
2020-12-15 19:56:29 +0800

    0756PM

---
+++ week07/scripts/components.js
@@ -0,0 +1,30 @@
+Vue.components("todo-item", {
+    prop: ["todo"],
+    template: "<li>{{todo}}</li>"
+});
+
+let app7 = new Vue ({
+    el: "#app7", 
+        data: {
+            groceryList:[
+                {
+                    id:0,
+                    text: "Vegetables"
+                },
+                {
+                    id:1,
+                    text: "Cheese"
+                },
+                {
+                    id:2,
+                    text: "Drinks"
+                },
+                {
+                    id:3,
+                    text: "Protiens"
+                }
+            ]
+        }
+
+});
+    
