1031c4b7a213949c85ca4eee0f2c98048de4876e
Josephine Phang <jphang75@gmail.com>
2020-12-15 21:44:31 +0800

    0944PM

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -28,3 +28,37 @@ let getBookingBtn = document.getElementById("getBooking");
 getBookingBtn.addEventListener("click", function(){
     GetBookings();
 });
+
+function BookNow(guestName, guestEmail, guestPax) {
+    let url = 'https://api.sheety.co/5ed1058f5a3d217f25701ff8f18f4b48/bookingApp/bookings'
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax
+        }
+    }
+    fetch(url, {
+        method: 'Post',
+        body: JSON.stringify(body)
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(json =>{
+        //Do something with object
+        //console.log(json.booking);
+        let bookMsg = document.getElementById("bookMsg");
+        bookMsg.innerHTML = json.booking.name + " added!"
+        GetBookings();
+    });
+}
+
+let bookNow = document.getElementById("bookNow");
+bookNow.addEventListener("click", function(){
+    let gName = document.getElementById("guestName").value;
+    let gEmail = document.getElementById("guestEmail").value;
+    let gPax = document.getElementById("guestPax").value;
+
+    BookNow(gName, gEmail, gPax);
+});
