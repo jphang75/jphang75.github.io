dd5f3751c846768f98d0a07ee9c1f6cc0019e279
Josephine Phang <jphang75@gmail.com>
2020-12-30 21:11:24 +0800

    0911PM

--- project/scripts/book.js
+++ project/scripts/book.js
@@ -18,7 +18,7 @@ function BookNow(guestName, guestEmail, guestDate, guestSlot, guestRemarks) {
     })
     .then((response) => response.json())
     .then(json =>{
-        alert(json.booking.name + " added!");
+        alert(json.booking.timeSlot + " added!");
     });
 }
 
